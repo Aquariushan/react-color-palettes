01a5eb25137adedc65b09c1d01023dac7bb0de01
Aquariushan <mxhcareer2022@gmail.com>
2022-04-15 08:54:14 -0700

    Updates

---
+++ precache-manifest.85bd8b9e2cffd3758163f066128c6e90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccdd240234354e10e5a0",
+    "url": "/react-color-palettes/static/css/main.cefeedaa.chunk.css"
+  },
+  {
+    "revision": "ccdd240234354e10e5a0",
+    "url": "/react-color-palettes/static/js/main.f58f4984.chunk.js"
+  },
+  {
+    "revision": "ddab485c698b7a212c03",
+    "url": "/react-color-palettes/static/js/runtime~main.5a50a9fd.js"
+  },
+  {
+    "revision": "82b5c3527500c4206114",
+    "url": "/react-color-palettes/static/css/2.a84caee4.chunk.css"
+  },
+  {
+    "revision": "82b5c3527500c4206114",
+    "url": "/react-color-palettes/static/js/2.44f102ac.chunk.js"
+  },
+  {
+    "revision": "10d86768450df61f717f",
+    "url": "/react-color-palettes/static/js/3.38f7337b.chunk.js"
+  },
+  {
+    "revision": "6b89e28dcf61123c50987dafe6139566",
+    "url": "/react-color-palettes/static/media/bg.6b89e28d.svg"
+  },
+  {
+    "revision": "0043fedea5db2332e9bf9de9bb7cea75",
+    "url": "/react-color-palettes/index.html"
+  }
+];
